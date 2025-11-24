dc6206e968d8bdf328c95e05b84bd2ea4eb08b0d
davidep <ernesto485pd@gmail.com>
2025-11-24 12:17:54 -0500

    Initial

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+@require_POST
+def logout_view(request):
+    logout(request)
+    return redirect('login')
